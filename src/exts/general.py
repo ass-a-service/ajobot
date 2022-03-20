48a52309dac6b6fc9ee566021be3e115a2f8fda9
vcokltfre <vcokltfre@gmail.com>
2022-03-20 09:17:32 +0000

    feat: add debug command

--- src/exts/general.py
+++ src/exts/general.py
@@ -1,7 +1,8 @@
 from disnake import CommandInteraction
-from disnake.ext.commands import Cog, slash_command
+from disnake.ext.commands import Cog, slash_command, command, is_owner, Context, Bot
 
 from src.impl.bot import Bot
+from src.impl.database import GarlicUser
 
 
 class General(Cog):
@@ -11,6 +12,16 @@ class General(Cog):
     @slash_command(name="support", description="Get an invite link to the support server.")
     async def support(self, itr: CommandInteraction) -> None:
         await itr.send("Here's an invite to my support server: https://discord.gg/t5Bs4cXfv2", ephemeral=True)
+
+    @command(name="debug")
+    @is_owner()
+    async def debug(self, ctx: Context[Bot]) -> None:
+        dbg = "\n".join([
+            f"DB users: {await GarlicUser.objects.count()}",
+            f"Latency: {self.bot.latency * 1000:.2f}ms",
+        ])
+
+        await ctx.send(dbg)
 
 
 def setup(bot: Bot) -> None:
