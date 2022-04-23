90a627b55d7e7abf075b33c3ed55c2b9b7c7eac4
Fabrizio Lamanna <fabriziolamanna18@gmail.com>
2022-04-24 00:44:20 +0200

    adding more logic

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,4 +1,13 @@
-redis-cli -x script load < src/lua/*.lua
+#!/bin/sh
+
+if [ -z ${REDIS_HOST} ]; then
+     echo -e "Redis host not defined, using local one"
+     export REDIS_HOST=localhost
+else
+    echo -e "Host is ${REDIS_HOST}"
+fi
+
+redis-cli -h ${REDIS_HOST} -x script load < src/lua/*.lua
 
 poetry run alembic upgrade head
 poetry run task start
