becc971716c57cc89a1ceefee005a97976c549e4
Axel Amigo Arnold <axl89@users.noreply.github.com>
2022-05-02 18:34:16 +0200

    Fix wrong redis invocation

--- src/lua/setne.lua
+++ src/lua/setne.lua
@@ -4,7 +4,7 @@ local name_key = KEYS[1]
 local called_name = ARGV[1]
 local current_name = redis.call("get", name_key)
 if called_name ~= current_name then
-    redis.call("set", called_name)
+    redis.call("set", name_key, called_name)
 end
 
 return "OK"
