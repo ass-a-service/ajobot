4c3cf637714eca0cfa3ff137345669bef79f729b
Axel Amigo Arnold <axl89@users.noreply.github.com>
2022-06-10 23:51:00 +0200

    Add roulette

--- entrypoint.sh
+++ entrypoint.sh
@@ -4,11 +4,16 @@ gamble=$(redis-cli -h ${REDIS_HOST} -x script load < src/lua/gamble.lua)
 pay=$(redis-cli -h ${REDIS_HOST} -x script load < src/lua/pay.lua)
 timely=$(redis-cli -h ${REDIS_HOST} -x script load < src/lua/timely_reward.lua)
 setne=$(redis-cli -h ${REDIS_HOST} -x script load < src/lua/setne.lua)
+roulette=$(redis-cli -h ${REDIS_HOST} -x script load < src/lua/roulette.lua)
+roulette_shot=$(redis-cli -h ${REDIS_HOST} -x script load < src/lua/roulette_shot.lua)
+
 
 grep DISCOMBOBULATE_SHA .env 1>/dev/null|| echo "DISCOMBOBULATE_SHA=$discombobulate" >> .env
 grep GAMBLE_SHA .env 1>/dev/null|| echo "GAMBLE_SHA=$gamble" >> .env
 grep PAY_SHA .env 1>/dev/null|| echo "PAY_SHA=$pay" >> .env
 grep TIMELY_SHA .env 1>/dev/null|| echo "TIMELY_SHA=$timely" >> .env
 grep SETNE_SHA .env 1>/dev/null|| echo "SETNE_SHA=$setne" >> .env
+grep ROULETTE_SHA .env 1>/dev/null|| echo "ROULETTE_SHA=$roulette" >> .env
+grep ROULETTE_SHOT_SHA .env 1>/dev/null|| echo "ROULETTE_SHOT_SHA=$roulette_shot" >> .env
 
 poetry run task start
