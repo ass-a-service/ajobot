44484fc01ff858623e41fa56cb1c6fe3c03e595e
Zymna <62969568+Zymna@users.noreply.github.com>
2022-06-17 20:11:08 +0200

    feat: earn gatos

--- src/lua/farm_inventory.lua
+++ src/lua/farm_inventory.lua
@@ -6,19 +6,20 @@ local id = tonumber(ARGV[1])
 local seed = tonumber(ARGV[2])
 
 -- implement here the items to potentially earn
-local item = ":cucumber:"
+local cucu = ":cucumber:"
 local gato = ":pouting_cat:"
 
 -- 25% chance to win a item
 math.randomseed(seed)
-if math.random(0, 3) ~= 1 then
+local rand = math.random(0, 3)
+if rand == 1 then
+    item = cucu
+elseif rand == 2 then
+    item = gato
+else
     return {"err", false}
 end
 
 local res = redis.call("hincrby", inventory_key, item, 1)
-if math.random(0, 3) ~= 1 then
-    redis.call("xadd", strm_key, "*", "user_id", id, "item", item, "quantity", 1)
-else
-    redis.call("xadd", strm_key, "*", "user_id", id, "item", gato, "quantity", 1)
-end
+redis.call("xadd", strm_key, "*", "user_id", id, "item", item, "quantity", 1)
 return {"OK", {item, res}}
