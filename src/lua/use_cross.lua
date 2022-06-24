24d4c0289a861893b95516e2dc3053aaae7ca12d
Zymna <62969568+Zymna@users.noreply.github.com>
2022-06-24 19:34:10 +0200

    fix: ability to have negative vampire levels

--- src/lua/use_cross.lua
+++ src/lua/use_cross.lua
@@ -10,7 +10,15 @@ if not stack or stack < 1 then
     return {"err", false}
 end
 
--- decrease stack and vampire level
+-- use the item, decrease stack
 redis.call("hdecrby", inventory_key, item, 1)
+
+-- if there is no vampire, you just used the item
+local vampire_level = tonumber(redis.call("get", vampire_key))
+if not vampire_level or vampire_level < 1 then
+    return {"OK", 0}
+end
+
+-- actually decrease stack and vampire level
 local res = redis.call("decrby", vampire_key, 1)
 return {"OK", res}
