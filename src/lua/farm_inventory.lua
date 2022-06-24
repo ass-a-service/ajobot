1fd0fb108ed03a3e6944b7faffe1b05e8ddb9547
Zymna <62969568+Zymna@users.noreply.github.com>
2022-06-24 17:36:45 +0200

    feat: implement loot table

--- src/lua/farm_inventory.lua
+++ src/lua/farm_inventory.lua
@@ -6,27 +6,28 @@ local id = tonumber(ARGV[1])
 local seed = tonumber(ARGV[2])
 
 -- implement here the items to potentially earn
-local chopsticks = ":chopsticks:"
-local cross = ":cross:"
-local item
+local items = {
+    ":sauropod:" = 1, -- 0.001,
+    ":chopsticks:" = 6, -- 0.006,
+    ":cross:" = 500, -- 0.5,
+    ":bomb:" = 200, -- 0.2
+}
 
 -- destellos / linterna
 math.randomseed(seed)
 local rand = math.random(0, 100000)
-if rand == 0 then
-    item = ":sauropod:" -- easter egg. It's just a beautiful dinosaur.
-elseif rand <= 5 then
-    item = chopsticks -- resets your vampire level to 0
-elseif rand <= 250 then
-    item = cross -- decrements your vampire level by 1
-elseif rand <= 300 then
-    item = ":bomb:" -- plants a bomb and a) the next person that farms will get the bomb or b) after x minutes (seteable) the last person that farmed will take the damage.
-elseif rand <= 500 then
-    item = "\xf0\x9f\x91\x9f" -- You farm twice as much for 5 minutes.
-else
-    return {"err", false}
+
+local acc = 0
+for item, chance in pairs(items) do
+    -- if we are lower than the current chance, get the item
+    if rand <= acc then
+        local res = redis.call("hincrby", inventory_key, item, 1)
+        redis.call("xadd", strm_key, "*", "user_id", id, "item", item, "quantity", 1)
+        return {"OK", {item, res}}
+    end
+
+    -- pass to the next item
+    acc = acc + chance
 end
 
-local res = redis.call("hincrby", inventory_key, item, 1)
-redis.call("xadd", strm_key, "*", "user_id", id, "item", item, "quantity", 1)
-return {"OK", {item, res}}
+return {"err", false}
