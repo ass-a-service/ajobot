f58bf3c1d64ca377dd769fee0f651f0cb5dfd7c9
Axel Amigo Arnold <axl89@users.noreply.github.com>
2022-06-24 18:04:43 +0200

    Minor changes to the test

--- util/test.py
+++ util/test.py
@@ -1,3 +1,5 @@
+#!/usr/bin/env python3
+
 import time, os, math
 import redis
 
@@ -5,9 +7,11 @@ r = redis.Redis(host='127.0.0.1')
 
 #r.flushdb(asynchronous=False)
 
+i=0
 while True:
-    seed_1 = str(time.time_ns()-(int(time.time())*1000000000))
+    seed_1 = time.time_ns()-(int(time.time())*1000000000)
     #seed_2 = str(int.from_bytes(os.urandom(16), 'big'))
     #seed_3 = seed_1[::-1]
-    result = r.evalsha('7f391805f220372f55f2ed3652a5d10082d75750', '2', "ajobus-inventory", "468516898309537792:inventory", "78313426", seed_1)
-    print(result)
+    result = r.evalsha('333d5850c1efbca2b2f7502d91a8d88873c209ee', '2', "ajobus-inventory", "468516898309537792:inventory", "78313426", seed_1)
+    print(f"{i}: {result}")
+    i = i+1
