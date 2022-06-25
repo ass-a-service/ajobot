d5491ca3047e249e560c9097ad28bcbe5e989ba8
Zymna <62969568+Zymna@users.noreply.github.com>
2022-06-25 18:45:24 +0200

    fix: variable namings

--- src/lua/trade.lua
+++ src/lua/trade.lua
@@ -6,7 +6,7 @@ local target_inv_key = KEYS[3]
 local source_id = ARGV[1]
 local target_id = ARGV[2]
 local item = ARGV[3]
-local decr_quantity = math.ceil(tonumber(ARGV[4]))
+local quantity = math.ceil(tonumber(ARGV[4]))
 
 -- FIXME: move this to redis maybe?
 local items = { -- max stack of items
@@ -34,20 +34,22 @@ if not source_stack or source_stack < 1 then
 end
 
 -- ensure we have enough items to trade
-if stack < quantity then
-    return {"funds", stack}
+if source_stack < quantity then
+    return {"funds", source_stack}
 end
 
 -- verify the target's max stack, max increase is minimised by stack
-local incr_quantity = decr_quantity
+local incr_quantity
 local target_stack = tonumber(redis.call("hget", target_inv_key, item))
-if target_stack > 0 then
-    incr_quantity = math.min(max_stack - target_stack, decr_quantity)
+if not target_stack or target_stack == 0 then
+    incr_quantity = quantity
+elseif target_stack > 0 then
+    incr_quantity = math.min(max_stack - target_stack, quantity)
 end
 
 -- decrease source stack, increase target stack, pass it to stream
-redis.call("hincrby", source_inv_key, item, -decr_quantity)
-redis.call("xadd", strm_key, "*", "user_id", source_id, "item", item, "quantity", -decr_quantity)
+redis.call("hincrby", source_inv_key, item, -quantity)
+redis.call("xadd", strm_key, "*", "user_id", source_id, "item", item, "quantity", -quantity)
 if incr_quantity > 0 then
     redis.call("hincrby", target_inv_key, item, incr_quantity)
     redis.call("xadd", strm_key, "*", "user_id", target_id, "item", item, "quantity", incr_quantity)
