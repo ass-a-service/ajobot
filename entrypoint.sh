751d2755539c2b1dbea852a30347d9eacea3879f
Zymna <62969568+Zymna@users.noreply.github.com>
2022-06-25 20:13:57 +0200

    fix: for entrypoint when missing keys

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,4 +1,6 @@
 #!/bin/sh
+set -euf
+
 discombobulate=$(redis-cli -h ${REDIS_HOST} -x script load < src/lua/discombobulate.lua)
 gamble=$(redis-cli -h ${REDIS_HOST} -x script load < src/lua/gamble.lua)
 pay=$(redis-cli -h ${REDIS_HOST} -x script load < src/lua/pay.lua)
@@ -10,6 +12,18 @@ farm_inventory=$(redis-cli -h ${REDIS_HOST} -x script load < src/lua/farm_invent
 use_chopsticks=$(redis-cli -h ${REDIS_HOST} -x script load < src/lua/use_chopsticks.lua)
 use_cross=$(redis-cli -h ${REDIS_HOST} -x script load < src/lua/use_cross.lua)
 craft_ajo_necklace=$(redis-cli -h ${REDIS_HOST} -x script load < src/lua/craft_ajo_necklace.lua)
+
+grep "DISCOMBOBULATE_SHA=" .env > /dev/null 2>&1 || echo "DISCOMBOBULATE_SHA=" >> .env
+grep "GAMBLE_SHA=" .env > /dev/null 2>&1 || echo "GAMBLE_SHA=" >> .env
+grep "PAY_SHA=" .env > /dev/null 2>&1 || echo "PAY_SHA=" >> .env
+grep "TIMELY_SHA=" .env > /dev/null 2>&1 || echo "TIMELY_SHA=" >> .env
+grep "SETNE_SHA=" .env > /dev/null 2>&1 || echo "SETNE_SHA=" >> .env
+grep "ROULETTE_SHA=" .env > /dev/null 2>&1 || echo "ROULETTE_SHA=" >> .env
+grep "ROULETTE_SHOT_SHA=" .env > /dev/null 2>&1 || echo "ROULETTE_SHOT_SHA=" >> .env
+grep "FARM_INVENTORY_SHA=" .env > /dev/null 2>&1 || echo "FARM_INVENTORY_SHA=" >> .env
+grep "USE_CHOPSTICKS_SHA=" .env > /dev/null 2>&1 || echo "USE_CHOPSTICKS_SHA=" >> .env
+grep "USE_CROSS_SHA=" .env > /dev/null 2>&1 || echo "USE_CROSS_SHA=" >> .env
+grep "CRAFT_AJO_NECKLACE_SHA=" .env > /dev/null 2>&1 || echo "CRAFT_AJO_NECKLACE_SHA=" >> .env
 
 sed -i "s/\(DISCOMBOBULATE_SHA\)=\([a-z0-9]\+\)\?/\1=$discombobulate/" .env
 sed -i "s/\(GAMBLE_SHA\)=\([a-z0-9]\+\)\?/\1=$gamble/" .env
