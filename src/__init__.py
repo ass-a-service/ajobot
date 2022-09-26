50457410eda0c927727eb8fff0039beac00cf094
Axel Amigo Arnold <axl89@users.noreply.github.com>
2022-09-26 06:58:08 +0200

    Added Sentry monitoring

--- src/__init__.py
+++ src/__init__.py
@@ -1,7 +1,18 @@
+from os import environ
 from dotenv import load_dotenv
+import sentry_sdk
 
 load_dotenv()
 
 from .impl import Bot
 
 __all__ = ("Bot",)
+
+sentry_sdk.init(
+    dsn=environ.get('SENTRY_DSN'),
+
+    # Set traces_sample_rate to 1.0 to capture 100%
+    # of transactions for performance monitoring.
+    # We recommend adjusting this value in production.
+    traces_sample_rate=1.0
+)
